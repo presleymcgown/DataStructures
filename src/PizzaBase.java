abcf0552d545c0b416c5164bdc085bdfd0205903
presley <presley.mcgown@icloud.com>
2022-10-24 08:31:15 -0600

    init

--- src/PizzaBase.java
+++ src/PizzaBase.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class PizzaBase {
+public abstract class PizzaBase {
+
+    public boolean hasCheese = true;
+    public boolean hasSauce = true;
+    public boolean hasToppings = true;
+    public int sliceCount = 8;
+
+    public void PizzaBase(boolean hasCheese, boolean hasSauce, boolean hasToppings, int sliceCount){
+
+        this.hasCheese = hasCheese;
+        this.hasSauce = hasSauce;
+        this.hasToppings = hasToppings;
+        this.sliceCount = sliceCount;
+
+    }
+
+    protected abstract void whatToppings();
+
+    protected abstract void cook();
+
+    public void finished(){
+
+        System.out.println("Your pizza is finished, enjoy!");
+
+    }
+
+
+
 }
